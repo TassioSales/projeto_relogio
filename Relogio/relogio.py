52c9ccc9019ea1b398706c2c772c574866b69e11
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-03-21 15:37:23 -0300

    modificaçoes do codigo

--- Relogio/relogio.py
+++ Relogio/relogio.py
@@ -2,17 +2,17 @@ import tkinter as tk
 import datetime
 
 
-class Telaprincipal():
+class Telaprincipal:
     def __init__(self, master):
         self.nossaTela = master
-        self.lblRelogio = tk.label(
-            self.nossaTela, font=("Calibri", 26), fg='Purble')
+        self.lblRelogio = tk.Label(
+            self.nossaTela, font=("Calibri", 26), fg='Purple')
         self.lblRelogio.pack(pady=30, padx=30)
         self.alteracao()
 
     def alteracao(self):
         now = datetime.datetime.now()
-        self.lblRelogio['Text'] = now.strftime('%H:%M:%S')
+        self.lblRelogio['text'] = now.strftime('%H:%M:%S')
         self.nossaTela.after(1000, self.alteracao)
 
 janelaRaiz = tk.Tk()
